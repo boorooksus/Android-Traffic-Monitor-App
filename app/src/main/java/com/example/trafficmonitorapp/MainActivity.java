247ef9063705456157ca104894258147ebd7657e
boorooksus <boorooksus@gmail.com>
2021-07-28 14:14:15 +0900

    NetworkStatsManager, Bucket 이용한 방식 - 에러

--- app/src/main/java/com/example/trafficmonitorapp/MainActivity.java
+++ app/src/main/java/com/example/trafficmonitorapp/MainActivity.java
@@ -36,37 +36,68 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            // 서버와 소켓 통신
-            try {
-
-                Socket socket = new Socket("192.168.0.7", 5000);
-
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
-                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-
-                dos.writeInt(0);
-
-                socket.close();
-            } catch (Exception e) {
-                System.out.println("================ Error =====================");
-                e.printStackTrace();
-            }
-
-            // current running app traffic
-            ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-            List<ActivityManager.RunningAppProcessInfo> runningApps = manager.getRunningAppProcesses();
-
-            for(ActivityManager.RunningAppProcessInfo runningApp : runningApps) {
-                Log.v("uid", runningApp.uid + "");
-                Log.v("send", TrafficStats.getUidTxBytes(runningApp.uid) + "");
-                Log.v("diff", (TrafficStats.getUidTxBytes(runningApp.uid) - last) + "");
-                last = TrafficStats.getUidTxBytes(runningApp.uid);
-
-            }
-
-            System.out.println("===================================================");
+//            // 서버와 소켓 통신
+//            try {
+//
+//                Socket socket = new Socket("192.168.0.7", 5000);
+//
+//                DataInputStream dis = new DataInputStream(socket.getInputStream());
+//                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+//
+//                dos.writeInt(0);
+//
+//                socket.close();
+//            } catch (Exception e) {
+//                System.out.println("================ Error =====================");
+//                e.printStackTrace();
+//            }
+//
+//            // current running app traffic
+//            ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+//            List<ActivityManager.RunningAppProcessInfo> runningApps = manager.getRunningAppProcesses();
+//
+//            for(ActivityManager.RunningAppProcessInfo runningApp : runningApps) {
+//                Log.v("uid", runningApp.uid + "");
+//                Log.v("send", TrafficStats.getUidTxBytes(runningApp.uid) + "");
+//                Log.v("diff", (TrafficStats.getUidTxBytes(runningApp.uid) - last) + "");
+//                last = TrafficStats.getUidTxBytes(runningApp.uid);
+//
+//            }
+//
+//            System.out.println("===================================================");
 
             // 설치된 모든 앱 정보
+
+//            PackageManager pm = getPackageManager();
+//            List<ApplicationInfo> apps = pm.getInstalledApplications(0);
+//
+//            for (ApplicationInfo app : apps) {
+//                // 설치된 앱들의 트래픽 - 이 앱 제외하고는 -1 리턴
+//                String appName = app.loadLabel(pm).toString();
+//                int uid = app.uid;
+//                long send = TrafficStats.getUidTxBytes(uid);
+//                long received = TrafficStats.getUidRxBytes(uid);
+//
+//                Log.v("" + uid , "Send :" + send + ", Received :" + received);
+//            }
+
+            NetworkStats networkStatsByApp;
+            NetworkStatsManager networkStatsManager =
+                    (NetworkStatsManager) getApplicationContext().getSystemService(Context.NETWORK_STATS_SERVICE);
+            try {
+                networkStatsByApp =
+                        networkStatsManager.querySummary(NetworkCapabilities.TRANSPORT_WIFI, "", 0, System.currentTimeMillis());
+                do {
+                    NetworkStats.Bucket bucket = new NetworkStats.Bucket();
+                    networkStatsByApp.getNextBucket(bucket);
+
+                    System.out.println("\nbucket: " + bucket.getUid());
+                    System.out.println("txBytes: " + bucket.getTxBytes());
+                } while (networkStatsByApp.hasNextBucket());
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
 
             PackageManager pm = getPackageManager();
             List<ApplicationInfo> apps = pm.getInstalledApplications(0);
@@ -79,20 +110,29 @@ public class MainActivity extends AppCompatActivity {
                 long send = TrafficStats.getUidTxBytes(uid);
                 long received = TrafficStats.getUidRxBytes(uid);
 
-                System.out.println(appName);
-                System.out.println("uid: " + app.uid);
-                System.out.println("send: " + send);
-                System.out.println("receive: " + received);
-                System.out.println(TrafficStats.getUidTxPackets(uid));
-                Log.v("" + uid , "Send :" + send + ", Received :" + received);
+                networkStatsByApp =
+                        networkStatsManager.queryDetailsForUid(NetworkCapabilities.TRANSPORT_WIFI,
+                                "",
+                                0,
+                                System.currentTimeMillis(),
+                                uid);
+//                do {
+//                    NetworkStats.Bucket bucket = new NetworkStats.Bucket();
+//                    networkStatsByApp.getNextBucket(bucket);
+//
+//                    System.out.println("bucket: " + bucket.getUid());
+//                    System.out.println("txBytes: " + bucket.getTxBytes());
+//                } while (networkStatsByApp.hasNextBucket());
 
+                //                NetworkStats.Bucket bucket = new NetworkStats.Bucket();
+//                networkStatsByApp.getNextBucket(bucket);
+//                System.out.println("bucket: " + bucket.getUid());
+//                System.out.println("txBytes: " + bucket.getTxBytes());
+                //Log.v("" + uid , "Send :" + send + ", Received :" + received);
 
             }
 
-            System.out.println("===============================");
-            // 전체 트래픽
-            System.out.println(TrafficStats.getTotalTxBytes());
-            System.out.println(TrafficStats.getTotalRxBytes());
+
 
 
             //super.run();
